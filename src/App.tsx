f8744808804b025f4e6a2cd75fb541904c0d1f7e
ferreiraalan13 <ferreiraalan13@gmail.com>
2025-07-22 19:20:53 -0300

    Configuração inicial do projeto e instalação de algumas bibliotecas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <div className="flex min-h-svh flex-col items-center justify-center ">
+        <Button className="bg-amber-200">
+          Configuração inicial do projeto
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
